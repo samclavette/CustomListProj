c202bc3ae1b115c37fa99358e0eaa41c7a329092
Sam Clavette <samclavette@yahoo.com>
2020-07-27 10:35:05 -0500

    I created the project, added the CustomLists class, added the testing class, and added a few test methods for the AddNumber method.

---
+++ CustomListTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CustomListTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CustomListTest")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a7d24e76-1188-4b1d-8c0c-a3d99fa8b647")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ CustomListTest/UnitTest1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CustomList;
+
+namespace CustomListTest
+{
+    [TestClass]
+    public class AddMethodsTest
+    {
+        [TestMethod]
+        public void AddMethod_AddOneInt_ListCountIsOne()
+        {
+            //Arrange
+            CustomList<int> list = new CustomList<int>();
+
+            int expected = 1;
+            int actual;
+
+            //Act
+
+            list.Add(5);
+            actual = list.Count;
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+
+        public void AddMethod_AddOneInt_Index0Equals5()
+        {
+            //Arrange
+            CustomList<int> list = new CustomList<int>();
+
+            int expected = 5;
+            int actual;
+
+            //Act
+            list.Add(5);
+            actual = list[0];
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+
+        public void AddMethod_AddOneInt_IndexEqualsNull()
+        {
+            //Arrange
+            CustomList<int> list = new CustomList<int>();
+
+            int expected = null;
+
+            //Act
+
+            //Assert
+        }
+    }
+}
